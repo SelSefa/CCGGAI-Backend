c9f0b556e8532c6308b57924a4a6cd0158089b73
Sefa Sel <sefasel@Sefa-Air.home>
2025-02-28 02:53:28 +0300

    Backend ilk commit

---
+++ src/main/java/CCGGAI/bau/project/backend_CCGGAI/BackendCcggaiApplication.java
@@ -0,0 +1,13 @@
+package CCGGAI.bau.project.backend_CCGGAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCcggaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCcggaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CCGGAI/bau/project/backend_CCGGAI/BackendCcggaiApplicationTests.java
@@ -0,0 +1,13 @@
+package CCGGAI.bau.project.backend_CCGGAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCcggaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
