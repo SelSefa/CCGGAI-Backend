99e5a8b459c32c061b2133073712e5dd75cb8e45
Sefa Sel <sefasel@Sefa-MacBook-Air.local>
2025-04-10 16:47:58 +0300

    Remove sensitive API key from application.properties

--- src/main/java/CCGGAI/bau/project/backend_CCGGAI/HomeController.java
+++ src/main/java/CCGGAI/bau/project/backend_CCGGAI/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/home")
-@CrossOrigin(origins = "http://localhost:3000") // React'in çalıştığı port
+@CrossOrigin(origins = "http://localhost:3000")
 public class HomeController {
     
     @GetMapping
